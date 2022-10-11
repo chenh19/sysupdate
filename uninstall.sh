2d20c19a4bcf363f66c584157f427489d82ce2e2
Hang Chen <chen_h@outlook.com>
2022-10-11 14:47:03 -0500

    Add files via upload

---
+++ uninstall.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# set terminal font color
+TEXT_YELLOW='\e[1;33m'
+TEXT_GREEN='\e[1;32m'
+TEXT_RESET='\e[0m'
+
+# confirm and remove
+echo ""
+read -n1 -s -r -p "$(echo -e $TEXT_YELLOW'Remove <sysupdate> command? [y/n]'$TEXT_RESET)"$' \n' choice
+case "$choice" in
+  y|Y ) # notify start
+        echo -e " \n${TEXT_YELLOW}Uninstalling <sysupdate> command... ${TEXT_RESET} \n" && sleep 1
+
+        # remove alias
+        if grep -q "alias sysupdate='bash ~/.update.sh'" ~/.bashrc ; then sed -i '/alias sysupdate=/d' ~/.bashrc ; fi
+        sleep 1
+
+        # remove shell scripts
+        [ -f ~/.update.R ] && rm ~/.update.R
+        [ -f ~/.update.sh ] && rm ~/.update.sh
+        sleep 1
+
+        # notify end
+        echo -e "${TEXT_GREEN}<sysupdate> command removed! Please reopen the terminal. ${TEXT_RESET} \n";;
+
+  * )   # notify end
+        echo -e " \n${TEXT_GREEN}<sysupdate> command not removed. ${TEXT_RESET} \n";;
+esac
