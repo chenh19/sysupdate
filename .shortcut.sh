4ac041ce70b42589590f75a2c6bcb1b2ea9bc480
Hang Chen <chen_h@outlook.com>
2023-02-14 01:18:39 -0600

    Update .shortcut.sh

--- .shortcut.sh
+++ .shortcut.sh
@@ -419,6 +419,92 @@
 /usr/share/applications/org.kde.Help.desktop
 
 
+#Flathub
+## clock
+[ ! -f ~/.local/share/applications/org.kde.kclock.desktop ] && touch ~/.local/share/applications/org.kde.kclock.desktop
+desktop-file-edit \
+    --set-name 'Clock' --set-key 'Name[en_US]' --set-value 'Clock' --set-key 'Name[zh_CN]' --set-value '时钟' \
+    --set-generic-name 'Clock Application' --set-key 'GenericName[en_US]' --set-value 'Clock Application' --set-key 'GenericName[zh_CN]' --set-value '时钟应用' \
+    --set-comment 'Set alarms and timers, use a stopwatch, and manage world clocks' --set-key 'Comment[en_US]' --set-value 'Set alarms and timers, use a stopwatch, and manage world clocks' --set-key 'Comment[zh_CN]' --set-value '设置闹钟，计时器，和世界时钟' \
+    --set-key 'Exec' --set-value '/usr/bin/flatpak run --branch=stable --arch=x86_64 --command=kclock org.kde.kclock' \
+    --set-icon 'org.kde.kclock' \
+    --set-key 'NoDisplay' --set-value 'false' \
+    --set-key 'Path' --set-value '' \
+    --set-key 'StartupNotify' --set-value 'true' \
+    --set-key 'Terminal' --set-value 'false' \
+    --set-key 'TerminalOptions' --set-value '' \
+    --set-key 'Type' --set-value 'Application' \
+    --set-key 'X-Flatpak' --set-value 'org.kde.kclock' \
+    --set-key 'X-KDE-FormFactor' --set-value 'desktop;tablet;handset;' \
+    --set-key 'X-KDE-SubstituteUID' --set-value 'false' \
+    --set-key 'X-KDE-Username' --set-value '' \
+    --remove-key 'Categories' --add-category 'Utility;' \
+~/.local/share/applications/org.kde.kclock.desktop
+
+## weather
+[ ! -f ~/.local/share/applications/org.kde.kweather.desktop ] && touch ~/.local/share/applications/org.kde.kweather.desktop
+desktop-file-edit \
+    --set-name 'Weather' --set-key 'Name[en_US]' --set-value 'Weather' --set-key 'Name[zh_CN]' --set-value '天气' \
+    --set-generic-name 'Weather Forecast' --set-key 'GenericName[en_US]' --set-value 'Weather Forecast' --set-key 'GenericName[zh_CN]' --set-value '天气预报' \
+    --set-comment 'View real-time weather forecasts and other information' --set-key 'Comment[en_US]' --set-value 'View real-time weather forecasts and other information' --set-key 'Comment[zh_CN]' --set-value '查看实时天气预报' \
+    --set-key 'Exec' --set-value '/usr/bin/flatpak run --branch=stable --arch=x86_64 --command=kweather org.kde.kweather' \
+    --set-icon 'org.kde.kweather' \
+    --set-key 'NoDisplay' --set-value 'false' \
+    --set-key 'Path' --set-value '' \
+    --set-key 'StartupNotify' --set-value 'true' \
+    --set-key 'Terminal' --set-value 'false' \
+    --set-key 'TerminalOptions' --set-value '' \
+    --set-key 'Type' --set-value 'Application' \
+    --set-key 'X-Flatpak' --set-value 'org.kde.kweather' \
+    --set-key 'X-KDE-FormFactor' --set-value 'desktop;tablet;handset;' \
+    --set-key 'X-KDE-SubstituteUID' --set-value 'false' \
+    --set-key 'X-KDE-Username' --set-value '' \
+    --remove-key 'Categories' --add-category 'Utility;' \
+~/.local/share/applications/org.kde.kweather.desktop
+
+## touché
+[ ! -f ~/.local/share/applications/com.github.joseexposito.touche.desktop ] && touch ~/.local/share/applications/com.github.joseexposito.touche.desktop
+desktop-file-edit \
+    --set-name 'Touché' --set-key 'Name[en_US]' --set-value 'Touché' --set-key 'Name[zh_CN]' --set-value '手势' \
+    --set-generic-name 'Touchégg Editor' --set-key 'GenericName[en_US]' --set-value 'Touchégg Editor' --set-key 'GenericName[zh_CN]' --set-value '觸控板手势' \
+    --set-comment 'Configure Touchégg multi-touch gestures' --set-key 'Comment[en_US]' --set-value 'Configure Touchégg multi-touch gestures' --set-key 'Comment[zh_CN]' --set-value '设置多指觸控手势' \
+    --set-key 'Exec' --set-value '/usr/bin/flatpak run --branch=stable --arch=x86_64 --command=com.github.joseexposito.touche com.github.joseexposito.touche' \
+    --set-icon 'com.github.joseexposito.touche' \
+    --set-key 'NoDisplay' --set-value 'true' \
+    --set-key 'Path' --set-value '' \
+    --set-key 'StartupNotify' --set-value 'true' \
+    --set-key 'Terminal' --set-value 'false' \
+    --set-key 'TerminalOptions' --set-value '' \
+    --set-key 'Type' --set-value 'Application' \
+    --set-key 'X-Flatpak' --set-value 'com.github.joseexposito.touche' \
+    --set-key 'X-KDE-FormFactor' --set-value 'desktop;tablet;handset;' \
+    --set-key 'X-KDE-SubstituteUID' --set-value 'false' \
+    --set-key 'X-KDE-Username' --set-value '' \
+    --remove-key 'Categories' --add-category 'Utility;' \
+~/.local/share/applications/com.github.joseexposito.touche.desktop
+
+## bottles
+[ ! -f ~/.local/share/applications/com.usebottles.bottles.desktop ] && touch ~/.local/share/applications/com.usebottles.bottles.desktop
+desktop-file-edit \
+    --set-name 'Bottles' --set-key 'Name[en_US]' --set-value 'Bottles' --set-key 'Name[zh_CN]' --set-value 'Bottles' \
+    --set-generic-name 'Wine Config Tool' --set-key 'GenericName[en_US]' --set-value 'Wine Config Tool' --set-key 'GenericName[zh_CN]' --set-value 'Wine编辑工具' \
+    --set-comment 'Run Windows Software' --set-key 'Comment[en_US]' --set-value 'Run Windows Software' --set-key 'Comment[zh_CN]' --set-value '运行Windows程序' \
+    --set-key 'Exec' --set-value '/usr/bin/flatpak run --branch=stable --arch=x86_64 --command=bottles --file-forwarding com.usebottles.bottles @@u %u @@' \
+    --set-icon 'com.usebottles.bottles' \
+    --set-key 'NoDisplay' --set-value 'false' \
+    --set-key 'Path' --set-value '' \
+    --set-key 'StartupNotify' --set-value 'true' \
+    --set-key 'Terminal' --set-value 'false' \
+    --set-key 'TerminalOptions' --set-value '' \
+    --set-key 'Type' --set-value 'Application' \
+    --set-key 'X-Flatpak' --set-value 'com.usebottles.bottles' \
+    --set-key 'X-KDE-FormFactor' --set-value 'desktop;tablet;handset;' \
+    --set-key 'X-KDE-SubstituteUID' --set-value 'false' \
+    --set-key 'X-KDE-Username' --set-value '' \
+    --remove-key 'Categories' --add-category 'Utility;' \
+~/.local/share/applications/com.usebottles.bottles.desktop
+
+
 #Hidden
 [ -f /usr/share/applications/cnsetuputil2l.desktop ] && sudo desktop-file-edit --set-key 'NoDisplay' --set-value 'true' /usr/share/applications/cnsetuputil2l.desktop
 [ -f /usr/share/applications/debian-uxterm.desktop ] && sudo desktop-file-edit --set-key 'NoDisplay' --set-value 'true' /usr/share/applications/debian-uxterm.desktop
