85fb3970545f3367c7914859ac2492adf6701091
Hang Chen <chen_h@outlook.com>
2024-04-12 21:02:44 -0500

    Update and rename update.sh to .update.sh

---
+++ .update.sh
@@ -0,0 +1,1 @@
+sudo Rscript -e "update.packages(ask = FALSE, checkBuilt = TRUE, Ncpus = system('nproc --all', intern = TRUE))" && echo ""
--- update.sh
+++
@@ -1,1 +0,0 @@
-sudo Rscript -e "update.packages(ask = FALSE, checkBuilt = TRUE, Ncpus = system(nproc --all, intern = TRUE))" && echo ""
