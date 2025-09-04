ab54cd02f05151eb94ce26880a2bf282168ce52b
Hang Chen <chen_h@outlook.com>
2025-09-03 21:28:00 -0500

    Update .size-restore.sh

--- .size-restore.sh
+++ .size-restore.sh
@@ -550,6 +550,46 @@ kwriteconfig5 --file ~/.local/state/kdevelopstaterc --group MainWindow --key '4 
 
 ######################################################################################
 
+# KDE Menu Editor
+## Window-Maximized
+### 3840x2400
+kwriteconfig5 --file ~/.local/state/kmenueditstaterc --group MainWindow --key '3840x2400 screen: Window-Maximized' "true" #1.0
+kwriteconfig5 --file ~/.local/state/kmenueditstaterc --group MainWindow --key '2560x1600 screen: Window-Maximized' "true" #1.5
+kwriteconfig5 --file ~/.local/state/kmenueditstaterc --group MainWindow --key '1920x1200 screen: Window-Maximized' "true" #2.0
+kwriteconfig5 --file ~/.local/state/kmenueditstaterc --group MainWindow --key '1536x960 screen: Window-Maximized' "true" #2.5
+### 3840x2160
+kwriteconfig5 --file ~/.local/state/kmenueditstaterc --group MainWindow --key '3840x2160 screen: Window-Maximized' "true" #1.0
+kwriteconfig5 --file ~/.local/state/kmenueditstaterc --group MainWindow --key '2560x1440 screen: Window-Maximized' "true" #1.5
+kwriteconfig5 --file ~/.local/state/kmenueditstaterc --group MainWindow --key '1920x1080 screen: Window-Maximized' "true" #2.0
+kwriteconfig5 --file ~/.local/state/kmenueditstaterc --group MainWindow --key '1536x864 screen: Window-Maximized' "true" #2.5
+### 3000x2000
+kwriteconfig5 --file ~/.local/state/kmenueditstaterc --group MainWindow --key '3000x2000 screen: Window-Maximized' "true" #1.0
+kwriteconfig5 --file ~/.local/state/kmenueditstaterc --group MainWindow --key '1500x1000 screen: Window-Maximized' "true" #2.0
+kwriteconfig5 --file ~/.local/state/kmenueditstaterc --group MainWindow --key '1200x800 screen: Window-Maximized' "true" #2.5
+### 3200x1800
+kwriteconfig5 --file ~/.local/state/kmenueditstaterc --group MainWindow --key '3200x1800 screen: Window-Maximized' "true" #1.0
+kwriteconfig5 --file ~/.local/state/kmenueditstaterc --group MainWindow --key '1600x900 screen: Window-Maximized' "true" #2.0
+kwriteconfig5 --file ~/.local/state/kmenueditstaterc --group MainWindow --key '1280x720 screen: Window-Maximized' "true" #2.5
+### 2880x1800
+kwriteconfig5 --file ~/.local/state/kmenueditstaterc --group MainWindow --key '2880x1800 screen: Window-Maximized' "true" #1.0
+kwriteconfig5 --file ~/.local/state/kmenueditstaterc --group MainWindow --key '1440x900 screen: Window-Maximized' "true" #2.0
+kwriteconfig5 --file ~/.local/state/kmenueditstaterc --group MainWindow --key '1152x720 screen: Window-Maximized' "true" #2.5
+### 2560x1600
+kwriteconfig5 --file ~/.local/state/kmenueditstaterc --group MainWindow --key '1707x1067 screen: Window-Maximized' "true" #1.5
+kwriteconfig5 --file ~/.local/state/kmenueditstaterc --group MainWindow --key '1280x800 screen: Window-Maximized' "true" #2.0
+kwriteconfig5 --file ~/.local/state/kmenueditstaterc --group MainWindow --key '1024x675 screen: Window-Maximized' "true" #2.5
+### 2560x1440
+kwriteconfig5 --file ~/.local/state/kmenueditstaterc --group MainWindow --key '1707x960 screen: Window-Maximized' "true" #1.5
+kwriteconfig5 --file ~/.local/state/kmenueditstaterc --group MainWindow --key '1024x640 screen: Window-Maximized' "true" #2.5
+### 2160x1350
+kwriteconfig5 --file ~/.local/state/kmenueditstaterc --group MainWindow --key '2160x1350 screen: Window-Maximized' "true" #1.0
+### multiple
+kwriteconfig5 --file ~/.local/state/kmenueditstaterc --group MainWindow --key '2 screens: Window-Maximized' "true" # 2 screens
+kwriteconfig5 --file ~/.local/state/kmenueditstaterc --group MainWindow --key '3 screens: Window-Maximized' "true" # 3 screens
+kwriteconfig5 --file ~/.local/state/kmenueditstaterc --group MainWindow --key '4 screens: Window-Maximized' "true" # 4 screens
+
+######################################################################################
+
 # Okular
 ## Window-Maximized
 ### 3840x2400
@@ -700,6 +740,7 @@ MATRIX=(
   "discoverstaterc"
   "kdenlivestaterc"
   "kdevelopstaterc"
+  "kmenueditstaterc"
   "okularstaterc"
   "systemsettingsstaterc"
 )
