16cc4c558f909ebcc8743af73cfc4c112b252119
Hang Chen <chen_h@outlook.com>
2025-09-06 13:10:57 -0500

    Update .size-restore.sh

--- .size-restore.sh
+++ .size-restore.sh
@@ -470,6 +470,46 @@ kwriteconfig6 --file ~/.local/state/discoverstaterc --group MainWindow --key '4 
 
 ######################################################################################
 
+# Gwenview
+## Window-Maximized
+### 3840x2400
+kwriteconfig6 --file ~/.local/state/gwenviewstaterc --group MainWindow --key '3840x2400 screen: Window-Maximized' --type bool "true" #1.0
+kwriteconfig6 --file ~/.local/state/gwenviewstaterc --group MainWindow --key '2560x1600 screen: Window-Maximized' --type bool "true" #1.5
+kwriteconfig6 --file ~/.local/state/gwenviewstaterc --group MainWindow --key '1920x1200 screen: Window-Maximized' --type bool "true" #2.0
+kwriteconfig6 --file ~/.local/state/gwenviewstaterc --group MainWindow --key '1536x960 screen: Window-Maximized' --type bool "true" #2.5
+### 3840x2160
+kwriteconfig6 --file ~/.local/state/gwenviewstaterc --group MainWindow --key '3840x2160 screen: Window-Maximized' --type bool "true" #1.0
+kwriteconfig6 --file ~/.local/state/gwenviewstaterc --group MainWindow --key '2560x1440 screen: Window-Maximized' --type bool "true" #1.5
+kwriteconfig6 --file ~/.local/state/gwenviewstaterc --group MainWindow --key '1920x1080 screen: Window-Maximized' --type bool "true" #2.0
+kwriteconfig6 --file ~/.local/state/gwenviewstaterc --group MainWindow --key '1536x864 screen: Window-Maximized' --type bool "true" #2.5
+### 3000x2000
+kwriteconfig6 --file ~/.local/state/gwenviewstaterc --group MainWindow --key '3000x2000 screen: Window-Maximized' --type bool "true" #1.0
+kwriteconfig6 --file ~/.local/state/gwenviewstaterc --group MainWindow --key '1500x1000 screen: Window-Maximized' --type bool "true" #2.0
+kwriteconfig6 --file ~/.local/state/gwenviewstaterc --group MainWindow --key '1200x800 screen: Window-Maximized' --type bool "true" #2.5
+### 3200x1800
+kwriteconfig6 --file ~/.local/state/gwenviewstaterc --group MainWindow --key '3200x1800 screen: Window-Maximized' --type bool "true" #1.0
+kwriteconfig6 --file ~/.local/state/gwenviewstaterc --group MainWindow --key '1600x900 screen: Window-Maximized' --type bool "true" #2.0
+kwriteconfig6 --file ~/.local/state/gwenviewstaterc --group MainWindow --key '1280x720 screen: Window-Maximized' --type bool "true" #2.5
+### 2880x1800
+kwriteconfig6 --file ~/.local/state/gwenviewstaterc --group MainWindow --key '2880x1800 screen: Window-Maximized' --type bool "true" #1.0
+kwriteconfig6 --file ~/.local/state/gwenviewstaterc --group MainWindow --key '1440x900 screen: Window-Maximized' --type bool "true" #2.0
+kwriteconfig6 --file ~/.local/state/gwenviewstaterc --group MainWindow --key '1152x720 screen: Window-Maximized' --type bool "true" #2.5
+### 2560x1600
+kwriteconfig6 --file ~/.local/state/gwenviewstaterc --group MainWindow --key '1707x1067 screen: Window-Maximized' --type bool "true" #1.5
+kwriteconfig6 --file ~/.local/state/gwenviewstaterc --group MainWindow --key '1280x800 screen: Window-Maximized' --type bool "true" #2.0
+kwriteconfig6 --file ~/.local/state/gwenviewstaterc --group MainWindow --key '1024x675 screen: Window-Maximized' --type bool "true" #2.5
+### 2560x1440
+kwriteconfig6 --file ~/.local/state/gwenviewstaterc --group MainWindow --key '1707x960 screen: Window-Maximized' --type bool "true" #1.5
+kwriteconfig6 --file ~/.local/state/gwenviewstaterc --group MainWindow --key '1024x640 screen: Window-Maximized' --type bool "true" #2.5
+### 2160x1350
+kwriteconfig6 --file ~/.local/state/gwenviewstaterc --group MainWindow --key '2160x1350 screen: Window-Maximized' --type bool "true" #1.0
+### multiple
+kwriteconfig6 --file ~/.local/state/gwenviewstaterc --group MainWindow --key '2 screens: Window-Maximized' --type bool "true" # 2 screens
+kwriteconfig6 --file ~/.local/state/gwenviewstaterc --group MainWindow --key '3 screens: Window-Maximized' --type bool "true" # 3 screens
+kwriteconfig6 --file ~/.local/state/gwenviewstaterc --group MainWindow --key '4 screens: Window-Maximized' --type bool "true" # 4 screens
+
+######################################################################################
+
 # Kdenlive
 ## Window-Maximized
 ### 3840x2400
@@ -738,6 +778,7 @@ done
 # maxmized windows [MainWindow]
 MATRIX=(
   "discoverstaterc"
+  "gwenviewstaterc"
   "kdenlivestaterc"
   "kdevelopstaterc"
   "kmenueditstaterc"
